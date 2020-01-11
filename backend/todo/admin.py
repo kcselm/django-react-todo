f737c963abf42f2fcfdfb8b5ab9528dd74ac4f58
kcselm <kcselm93@gmail.com>
2020-01-11 10:52:31 -0600

    updating indents

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 from django.contrib import admin
-    from .models import Todo # add this
+from .models import Todo # add this
 
-    class TodoAdmin(admin.ModelAdmin):  # add this
-      list_display = ('title', 'description', 'completed') # add this
+class TodoAdmin(admin.ModelAdmin):  # add this
+    list_display = ('title', 'description', 'completed') # add this
 
-    # Register your models here.
-    admin.site.register(Todo, TodoAdmin) # add this
+# Register your models here.
+admin.site.register(Todo, TodoAdmin) # add this
