61866edb486a1cf9e8597ea6bce8a4a133d07773
kcselm <kcselm93@gmail.com>
2020-01-11 12:31:41 -0600

    Debugging views and cors errors

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,8 +19,9 @@ from rest_framework import routers
 from todo import views 
 
 router = routers.DefaultRouter()
-router.register(r'todos', view.todoView, 'todo')
+router.register(r'todos', views.TodoView, 'todo')
 
 urlpatterns = [
-    path('admin/', admin.site.urls), path('api/', include(router.urls))
+    path('admin/', admin.site.urls), 
+    path('api/', include(router.urls))
 ]
